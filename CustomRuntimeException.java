2001e4a86c3b5fb717b4f1eb97b7f63d3f82b856
garimatiwari014-stack <garimatiwari014@gmail.com>
2026-07-08 03:05:59 +0530

    Add files via upload

---
+++ CustomRuntimeException.java
@@ -0,0 +1,25 @@
+class YoungerAgeException extends RuntimeException{
+    YoungerAgeException(){
+        super("You are not eligible");
+    }
+    YoungerAgeException(String msg){
+        super(msg);
+
+    }
+}
+class votingEligible{
+    public static void main(String[]args){
+        int age = 16 ;
+        try{
+            if(age < 18){
+                throw new YoungerAgeException("You are not eligible for voting");
+            }
+            else{
+                System.out.println("You are eligible for voting ");
+            }
+        }
+        catch(YoungerAgeException e){
+            System.out.println(e.getMessage());
+        }
+        
+    }}
